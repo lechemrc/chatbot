890837b42c0847ecff656fa7117a643fa4a5e199
Rob LeCheminant <roblecheminant@gmail.com>
2020-06-28 21:59:37 -0600

    adding detailed chatbot files

--- gui_chatbot.py
+++ gui_chatbot.py
@@ -1,13 +1,13 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import pickle
-import numpy as np
-
-from keras.models import load_model
-model = load_model('chatbot_model.h5')
 import json
 import random
+import pickle
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot_model.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
